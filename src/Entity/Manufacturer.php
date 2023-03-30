5516bedcfe3f5fca3bc23d51cbd45bb7e9c07d42
adnanahmady <adnan.ahmady1394@gmail.com>
2023-03-30 14:42:54 +0330

    Feature: manufacturer is added

---
+++ src/Entity/Manufacturer.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource]
+#[ORM\Entity]
+class Manufacturer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private string $name = '';
+
+    #[ORM\Column(type: 'text')]
+    private string $description = '';
+
+    #[ORM\Column(length: 3)]
+    private string $countryCode = '';
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $listedDate = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode(): string
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryCode
+     */
+    public function setCountryCode(string $countryCode): void
+    {
+        $this->countryCode = $countryCode;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getListedDate(): ?\DateTimeInterface
+    {
+        return $this->listedDate;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $listedDate
+     */
+    public function setListedDate(?\DateTimeInterface $listedDate): void
+    {
+        $this->listedDate = $listedDate;
+    }
+}
